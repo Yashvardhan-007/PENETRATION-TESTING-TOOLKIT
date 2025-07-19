f643859941ddac6134cbae62f9572237580de0c1
Yashvardhan Singh <chanchalyashsingh2005@gmail.com>
2025-07-19 21:40:32 +0530

    Update port_scanner.py

--- modules/port_scanner.py
+++ modules/port_scanner.py
@@ -1,14 +1,16 @@
 import socket
+from utils import logger
 
 def scan_ports(target, ports):
-    print(f"\n[*] Scanning {target}")
+    logger.info(f"Scanning {target}")
     for port in ports:
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
             result = sock.connect_ex((target, port))
             if result == 0:
-                print(f"[+] Port {port} is OPEN")
+                logger.info(f"Port {port} is OPEN")
             sock.close()
         except Exception as e:
-            print(f"[-] Error scanning port {port}: {e}")
+            logger.error(f"Error scanning port {port}: {e}")
+
